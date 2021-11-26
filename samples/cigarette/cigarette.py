8c1ed2c926331bb993a2f71d5a11241ddc1c0be3
aziz <aziz.oumsalem@outlook.fr>
2021-11-26 22:05:51 +0100

    [update] STEPS_PER_EPOCH to 100

--- samples/cigarette/cigarette.py
+++ samples/cigarette/cigarette.py
@@ -69,7 +69,7 @@ class CigaretteConfig(Config):
     NUM_CLASSES = 1 + 1  # Background + cigarette
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 1
+    STEPS_PER_EPOCH = 100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
